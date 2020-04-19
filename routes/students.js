e669ab408bbef1e8a7b08ffb7aeab3101e0d1e16
themagiche <magiche.mc@gmail.com>
2020-04-20 00:13:53 +0300

    fixed registration issue

--- routes/students.js
+++ routes/students.js
@@ -20,9 +20,10 @@ router.get('/classes/:id', async function(req, res, next) {
         }).populate({
             path: 'classes',
         });
+
         // console.log(studentByUsername.classes);
 
-        res.status(200).json({ classes: studentByUsername.classes });
+        return res.status(200).json({ classes: studentByUsername.classes });
     } catch (err) {
         console.log(err);
     }
@@ -40,20 +41,33 @@ router.post('/:id/class/:classID/register', async function(req, res) {
         const { id, classID } = req.params;
 
         const studentByID = await User.findById(id);
-        const studentByUsername = await Student.findOne({
-            username: studentByID.username,
-        });
         const classByID = await Class.findById(classID);
+        var query = { username: studentByID.username };
+        let studentByUsername = await Student.findOneAndUpdate(
+            query,
+            // Append the values to an array - push classes inside the instructor collection
+            { $addToSet: { classes: classByID } },
+            // Creates a new document if no documents match the filter or update
+            { safe: true, upsert: true },
+            function(err) {
+                if (err) {
+                    return res.json({
+                        msg: 'Already registered for class',
+                        success: false,
+                    });
+                } else {
+                    return res.status(200).json({
+                        msg: 'Registered class successfully',
+                        success: true,
+                    });
+                }
+            }
+        );
 
-        console.log(studentByID);
+        // console.log(studentByUsername);
+        // studentByUsername.classes.push(classByID);
 
-        studentByUsername.classes.push(classByID);
-
-        studentByUsername.save();
-
-        res.status(200).json({
-            msg: 'Succesfully registered class',
-        });
+        // studentByUsername.save();
     } catch (err) {
         console.log(err);
     }
