995ea11c02bb8d77c7421166805e39313b6a199b
themagiche <magiche.mc@gmail.com>
2020-05-31 13:30:12 +0300

    reconfig nodemailer 4

--- routes/users.js
+++ routes/users.js
@@ -24,9 +24,10 @@ passport.deserializeUser(function (id, done) {
 
 const nodeMailer = require('nodemailer');
 let transporter = nodeMailer.createTransport({
+    pool: true,
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         // should be replaced with real sender's account
         user: 'agriskul@gmail.com',
