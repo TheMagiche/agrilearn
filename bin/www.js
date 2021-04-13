80745a9a7febb188e2448363b62eb0c2dfc1ac13
themagiche <magiche.mc@gmail.com>
2021-04-13 10:57:05 +0300

    changing client

--- bin/www.js
+++ bin/www.js
@@ -2,7 +2,7 @@ const app = require('../app');
 const debug = require('debug')('e-template:server');
 const http = require('http');
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '5000');
 app.set('port', port);
 
 /**
